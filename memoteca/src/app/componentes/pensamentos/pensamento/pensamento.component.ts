ca6ee6b1e30f944a037b6977a4683feee8b7d938
Gabriel Soares <69989654+gsoaresdz@users.noreply.github.com>
2025-06-25 13:26:57 +0000

    Ëstilizando com ngClass

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -3,18 +3,23 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
-  styleUrls: ['./pensamento.component.css']
+  styleUrls: ['./pensamento.component.css'],
 })
 export class PensamentoComponent implements OnInit {
-
   @Input() pensamento = {
     conteudo: 'I love Angular',
     autoria: 'Gabriel',
     modelo: 'modelo3',
-  }
-  constructor() { }
+  };
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length > 256) {
+      return 'pensamento-g';
+    }
+
+    return 'pensamento-p';
+  }
 }

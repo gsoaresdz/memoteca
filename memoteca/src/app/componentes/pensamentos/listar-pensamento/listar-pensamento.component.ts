89d2449a02d3cab8ba7e4f6ac885e5b772a8dd59
Gabriel Soares <69989654+gsoaresdz@users.noreply.github.com>
2025-09-01 15:11:02 +0000

    Excluindo um favorito da lista

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -13,6 +13,7 @@ export class ListarPensamentoComponent implements OnInit {
   haMaisPensamentos: boolean = true;
   filtro: string = '';
   favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = [];
 
   constructor(private service: PensamentoService) {}
 
@@ -45,6 +46,7 @@ export class ListarPensamentoComponent implements OnInit {
     this.paginaAtual = 1;
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe(listaPensamentosFavoritos => {
       this.listaPensamentos = listaPensamentosFavoritos;
+      this.listaFavoritos = listaPensamentosFavoritos;
     })
   }
 }
